c0aad613f1f9ce201ff7cf78b86947544993acf9
Yash Puranik <50245579+YashPuranik27@users.noreply.github.com>
2021-08-22 15:40:59 -0400

    Initial commit

---
+++ stockBot.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import discord
+from discord.ext import commands
+import utils
+import marketAPI as api
+import re
+import os
+
+TOKEN = os.environ['TOKEN']
+pattern_quote = re.compile(r'[$]([A-Za-z]+)[+]?')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        await client.change_presence(status=discord.Status.online, activity=discord.Game("with Stonks"))
+
+    async def on_message(self, message: discord.message.Message):
+        if message.author.id != client.user.id:  # fix recursive spam
+            matches = re.findall(pattern_quote, message.content)
+            matches_len = len(matches)
+
+            if utils.should_parse_message(matches_len):
+                for m in matches:
+                    if m[-1] == '+':
+                        embed = api.get_extended_quote(m)
+                        await message.channel.send(embed=embed)
+                    else:
+                        embed = api.get_basic_quote(m)
+                        await message.channel.send(embed=embed)
+
+            else:
+                if matches_len > 0:
+                    await message.channel.send("Market is closed.")
+                else:
+                    
+
+client = MyClient(#private discord client number)
+client.run(#insert your unique client token here)
---
+++ utils.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+
+
+def is_market_open() -> bool:
+    """
+    Does some quick mafs to check if the stonk market is open
+    :return: bool
+    """
+    now = datetime.now()
+    return (now.weekday() < 5) and (5 <= (now.hour + (now.minute / 60)) <= 14)
+
+
+def is_pre_market() -> bool:
+    """
+    Does some quick mafs to check if it's pre-market time
+    :return: bool
+    """
+    now = datetime.now()
+    return 5 <= (now.hour + (now.minute / 60)) <= 6.5
+
+
+def is_after_hours() -> bool:
+    """
+    Does some quick mafs to check if it's after-hours gambling time
+    :return: bool
+    """
+    now = datetime.now()
+    return 13 <= (now.hour + (now.minute / 60)) <= 14
+
+
+def should_parse_message(matches_len: int) -> bool:
+    """
+    Determines if a discord message should be parsed. We only
+    parse up to and including 5 symbols per message
+    :param matches_len: length of re.findall() resultant list
+    :return: bool
+    """
+
+    return is_market_open() and 0 < matches_len < 6
